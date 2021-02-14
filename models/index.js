b18049807aadec97a9e6b918511f18b04fc34d08
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2021-02-14 14:55:06 +1100

    JAWS_DB connection

--- models/index.js
+++ models/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 "use strict";
 
 const fs = require('fs');
@@ -5,11 +6,19 @@ const path = require('path');
 const { Sequelize, DataTypes } = require('sequelize');
 const mysql = require('mysql')
 
-
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  const db = {};
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+  host: config.host,
+  port: config.port,
+  dialect: 'mysql',
+});
+} else {
 const basename = path.basename(module.filename);
 const config = {
   username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '.',
+  password: process.env.DB_PASSWORD || 'Queenw00d.',
   database: process.env.DB_NAME || 'teamassist',
   host: process.env.DB_HOSTNAME || 'localhost',
   port: process.env.DB_PORT || 3306,
@@ -36,20 +45,17 @@ Object.keys(db).forEach((modelName) => {
   }
 });
 
-
 // JOINING TABLES FOR MEMBER
 db.Member.hasMany(db.Record, { foreignKey: 'member_id' })
 db.Record.belongsTo(db.Member, { foreignKey: 'member_id' })
-
 
 // // JOINING TABLES FOR CERT
 db.Cert.hasMany(db.Record, { foreignKey: 'cert_id' })
 db.Record.belongsTo(db.Cert, { foreignKey: 'cert_id' })
 
-
 sequelize.sync()
-
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
 
 module.exports = db;
+}
