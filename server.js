eb59994af62cd87bff5f18647be0eefd17c8597a
DukeWilki <65857753+DukeWilki@users.noreply.github.com>
2021-02-15 08:38:18 +1100

    require path

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const session = require("express-session");
+const path = require('path');
 // const bodyParser = require("body-parser"); // form data   // multer
 // const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
